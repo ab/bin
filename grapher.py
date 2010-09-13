8502b597fb45cea82b00068323a4832e3790cc02
Andy Brody <andy@abrody.com>
2010-09-13 00:38:23 -0400

    Import slightly newer grapher.py.

--- grapher.py
+++ grapher.py
@@ -5,6 +5,8 @@ import re
 import sys
 from subprocess import call, Popen, PIPE
 
+VERSION = '0.1'
+
 def get_term_size():
     p = Popen(['tput', '-S'], stdin=PIPE, stdout=PIPE)
     out = p.communicate('lines\ncols\n')[0]
@@ -38,11 +40,16 @@ def graph_loop(*args, **kwargs):
     call('clear')
     graph(*args, **kwargs)
 
-def curses_graph(stdscr, stream, width=None, height=None, char='|', scale=True):
+def curses_graph(stdscr, stream, width=None, height=None, char='|',
+                 scale=True, strict_input=False):
     if width is None:
         width = stdscr.getmaxyx()[1]
     if height is None:
         height = stdscr.getmaxyx()[0]
+
+    # don't display a flashing cursor, don't block waiting for keyboard input
+    curses.curs_set(0)
+    stdscr.nodelay(1)
 
     vmax = 0
     values = []
@@ -53,7 +60,13 @@ def curses_graph(stdscr, stream, width=None, height=None, char='|', scale=True):
         try:
             v = int(line)
         except ValueError:
-            continue
+            try:
+                v = float(line)
+            except ValueError:
+                if strict_input:
+                    raise
+                else:
+                    continue
         values.append(v)
         if v > vmax:
             vmax = v
@@ -72,13 +85,30 @@ def curses_graph(stdscr, stream, width=None, height=None, char='|', scale=True):
             if scaled <= 0:
                 continue
             stdscr.vline(height - scaled, col, char, scaled)
+
+        # label max value
+        stdscr.addstr(0, 0, str(vmax))
+
         stdscr.refresh()
+
+        # check for keyboard input, allow pausing (NB: nodelay enabled above)
+        # also note that this will NOT work when input is piped to stdin
+        c = stdscr.getch()
+        if c != -1:
+            stdscr.refresh()
+            stdscr.nodelay(0)
+            stdscr.addstr(1, 0, "[PAUSED]")
+            stdscr.getch()  # unpause on any keypress
+            stdscr.nodelay(1)
+        
 
 if __name__ == '__main__':
     usage = 'usage: %prog [options] {FILENAME | -}'
     p = optparse.OptionParser(usage)
     p.add_option('-s', '--stream', dest='stream', action='store_true',
                  help='draw a streaming graph')
+    p.add_option('-e', '--error-mode', dest='strict_input', action='store_true',
+                 help='raise an error on invalid input data')
     (options, args) = p.parse_args()
 
     try:
@@ -92,16 +122,27 @@ if __name__ == '__main__':
     else:
         f = open(fname)
 
-    try:
-        if options.stream:
-            # use curses for streaming graph output
-            curses.wrapper(curses_graph, f)
-        else:
-            values = []
-            for line in f:
-                values.append(int(line))
-            f.close()
-            graph(values)
-    except KeyboardInterrupt:
-        print ''
+    if options.stream:
+        # use curses for streaming graph output
+        try:
+            curses.wrapper(curses_graph, f, strict_input=options.strict_input)
+        except KeyboardInterrupt:
+            print '^C',
+    else:
+        values = []
+        for line in f:
+            try:
+                v = int(line)
+            except ValueError:
+                try:
+                    v = float(line)
+                except ValueError:
+                    if options.strict_input:
+                        raise
+                    else:
+                        continue
+            values.append(v)
+
+        f.close()
+        graph(values)
 
