361019c8688091b63c3d359ebceba10411aad3cd
Andy Brody <andy@abrody.com>
2012-07-18 02:33:37 -0700

    Add script to format times for beeminder.

---
+++ beeminder.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import sys
+from datetime import datetime
+
+def parse_date(line):
+    prefix = '%Y-%m!'
+    line = datetime.now().strftime(prefix) + line
+    fmt = prefix + '%d %H:%M'
+    return datetime.strptime(line, fmt)
+
+data = []
+while True:
+    line = raw_input('day hour:min -- ').strip()
+    if not line:
+        break
+
+    date = parse_date(line)
+    hours = date.hour + date.minute / 60.0
+    data.append((date.day, hours, date.strftime('%F %R')))
+
+for row in data:
+    sys.stderr.write('%s %f "%s"\n' % row)
