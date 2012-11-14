acc0ae911dcb45b3514548ae4c43ed23dcf0a59d
Andy Brody <andy@stripe.com>
2012-11-14 00:41:25 -0800

    aws-data1 is now data.w1!

--- apiori-db-ids.sh
+++ apiori-db-ids.sh
@@ -8,7 +8,7 @@ query="select id from cards where created < $timestamp order by id"
 tmpd="$(mktemp -d)"
 trap "rm -rf '$tmpd'" EXIT
 
-for host in apiori-db{2,4,-e1,-e2} aws-data1 data.{w2,e1,e3}; do
+for host in apiori-db{2,4,-e1,-e2} data.{w1,w2,e1,e3}; do
     ssh -C $host "mysql -e '$query' cards | md5sum" > $tmpd/$host.md5 &
 done
 
@@ -16,7 +16,7 @@ wait
 
 set +x
 
-for host in apiori-db{2,4,-e1,-e2} aws-data1 data.{w2,e1,e3}; do
+for host in apiori-db{2,4,-e1,-e2} data.{w1,w2,e1,e3}; do
     cut -f1 -d' ' $tmpd/$host.md5 | tr -d '\n'
     echo " $host"
 done
