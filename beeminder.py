a94e60ad9959748e80c9d1b9aafc6724ec2bc78f
Andy Brody <andy@abrody.com>
2013-12-26 03:54:42 -0500

    Refactor beeminder.py and accept comments.
    - Use stdout instead of stderr for output, and don't print a message for
      every line read.
    - Accept arbitrary extra stuff at the end of the line and include it as
      a comment. The main use case here is to add a timezone, which we
      aren't able to parse without a lot of additional work.

--- beeminder.py
+++ beeminder.py
@@ -4,24 +4,41 @@ from datetime import datetime
 
 def parse_date(line):
     prefix = '%Y-%m!'
-    line = datetime.now().strftime(prefix) + line
+    if len(line.split(' ', 2)) > 2:
+        one, two, comment = line.split(' ', 2)
+        time_part = one + ' ' + two
+    else:
+        comment = None
+        time_part = line
+
+    time_part = datetime.now().strftime(prefix) + time_part
+
     fmt = prefix + '%d %H:%M'
-    return datetime.strptime(line, fmt)
+    try:
+        return datetime.strptime(time_part, fmt), comment
+    except ValueError:
+        sys.stderr.write("Failed to parse %r" % line)
+        raise
+
+if sys.stdin.isatty():
+    sys.stderr.write('format: day hour:min [comment]\n')
 
 data = []
 while True:
     try:
-        line = raw_input('day hour:min -- ').strip()
+        line = sys.stdin.readline().strip()
     except EOFError:
-        print ''
         break
-
     if not line:
         break
 
-    date = parse_date(line)
+    date, comment = parse_date(line)
     hours = date.hour + date.minute / 60.0
-    data.append((date.day, hours, date.strftime('%F %R')))
+
+    processed_input = date.strftime('%F %R')
+    if comment:
+        processed_input += ' ' + comment
+    data.append((date.day, hours, processed_input))
 
 for row in data:
-    sys.stderr.write('%s %f "%s"\n' % row)
+    print '%s %f "%s"' % row
