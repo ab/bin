57912d756fcd66f2b64bc6277d37d917e639175d
Andy Brody <andy@abrody.com>
2014-02-17 01:43:28 -0800

    Rework diceware implementation and print entropy.

--- diceware.py
+++ diceware.py
@@ -1,46 +1,73 @@
 #!/usr/bin/env python
 
+import math
 import random
+import string
 import sys
 
 # SystemRandom is a subclass of random that uses os.urandom
-randrange = random.SystemRandom().randrange
+systemrandom = random.SystemRandom()
 
-def getlist(fname):
-    f = open(fname, 'r')
-    d = ['']
+# TODO: use optparse
+punctuation = False
 
-    for line in f:
-        word = line.strip()
+def getlist_words(fname):
+    words = []
+    with open(fname, 'r') as f:
+        previous = None
 
-        # skip "" and "...'s"
-        if not word or word == d[-1] + "'s":
-            continue
+        for line in f:
+            word = line.strip()
 
-        # strip index, if any:
-        if '\t' in word:
-            x = word.split('\t')
-            if len(x) == 2 and x[0].isdigit():
-                word = x[1]
+            if not word:
+                continue
 
-        d.append(word)
+            # strip 's suffixes
+            if previous and word == previous + "'s":
+                continue
 
-    f.close()
+            words.append(word)
+            previous = word
 
-    d.remove('')
-    return d
+    return words
+
+def getlist_diceware(fname):
+    words = []
+    with open(fname, 'r') as f:
+        for line in f:
+            word = line.strip()
+
+            if not word:
+                continue
+
+            # strip index
+            try:
+                index, word = word.split('\t')
+            except ValueError:
+                sys.stderr.write("Bad line format? Expected index<tab>word, not " +
+                                 repr(line) + "\n")
+                raise
+            assert index.isdigit(), 'index should consist of digits'
+
+            words.append(word)
+
+    return words
+
+def getlist_auto(fname):
+    if '\t' in open(fname, 'r').readline():
+        return getlist_diceware(fname)
+    else:
+        return getlist_words(fname)
 
 def getwords(wordlist, num=1):
-    words = getlist(wordlist)
-    maxword = len(words)
+    dictionary = getlist_auto(wordlist)
+    return [pair_from_word_list(dictionary) for i in range(num)]
 
-    result = []
-
-    for i in range(num):
-        result.append(words[randrange(maxword)])
-
-    return result
-
+def pair_from_word_list(word_list):
+    """
+    Get a random word and the entropy it represents from the given word_list.
+    """
+    return systemrandom.choice(word_list), math.log(len(word_list), 2)
 
 if __name__ == '__main__':
     try:
@@ -52,6 +79,22 @@ if __name__ == '__main__':
     except IndexError:
         fname = '/usr/share/dict/words'
 
-    sys.stderr.write("Using '%s'.\n" % fname)
-    print ' '.join(getwords(fname, num))
+    sys.stderr.write("Dictionary: %r\n" % fname)
+    words = getwords(fname, num)
+    if punctuation:
+        punc_words = []
+        for word in words[:-1]:
+            punc_words.append(word)
+            punc_words.append(pair_from_word_list(string.punctuation))
+        punc_words.append(words[-1])
 
+        words = punc_words
+        sep = ''
+    else:
+        sep = ' '
+
+    entropy = sum(pair[1] for pair in words)
+
+    sys.stderr.write("Entropy: ~%.1f bits\n" % round(entropy, 1))
+
+    print sep.join(pair[0] for pair in words)
