f56c9e685b5bba7c365ef264b07e7a94b89a4c16
Andy Brody <git@abrody.com>
2015-07-29 20:22:21 -0400

    Mkdir mount moint in install.sh.

--- ramdisk/install.sh
+++ ramdisk/install.sh
@@ -4,6 +4,7 @@ set -eu
 cd "$(dirname "$0")"
 
 mkdir -vp /etc/ramdisk.d
+mkdir -vp /mnt/ramdisk
 cp -v create-userdirs /etc/ramdisk.d/
 cp -v mount-ramdisk /etc/init.d/
 update-rc.d mount-ramdisk defaults
